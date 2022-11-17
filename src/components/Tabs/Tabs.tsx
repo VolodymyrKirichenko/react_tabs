1de101396d597b3b09699acf45f94ba490786ce4
Sergio k <valodya.kirik@gmail.com>
2022-11-17 20:19:45 +0200

    project has been added

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,51 @@
-export const Tabs = () => {};
+import { FC } from 'react';
+import cn from 'classnames';
+import { Tab } from '../../types/Tab';
+
+interface Props {
+  tabs: Tab[];
+  selectedTabId: string;
+  setSelectedTab: (tab: Tab) => void;
+}
+
+export const Tabs: FC<Props> = (props) => {
+  const { tabs, selectedTabId, setSelectedTab } = props;
+
+  const isActive = tabs.find(tab => tab.id === selectedTabId) || tabs[0];
+
+  const handleClick = (tab: Tab) => {
+    if (tab.id !== isActive.id) {
+      setSelectedTab(tab);
+    }
+  };
+
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              data-cy="Tab"
+              className={cn({
+                'is-active': tab.id === isActive.id,
+              })}
+            >
+              <a
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+                onClick={() => handleClick(tab)}
+              >
+                {tab.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+
+        <div className="block" data-cy="TabContent">
+          {isActive.content}
+        </div>
+      </div>
+    </div>
+  );
+};
